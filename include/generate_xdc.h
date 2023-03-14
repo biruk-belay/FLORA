e4e0eb6692a76b2e09c309a4545461b6c0b311c1
biruk <forbiruk@gmail.com>
2023-03-14 17:49:01 -0400

    fixed minor bug

--- include/generate_xdc.h
+++ include/generate_xdc.h
@@ -65,16 +65,16 @@ string slot_names[] = {"slot_0", "slot_1", "slot_2", "slot_3",
         write_xdc << "resize_pblock [get_pblocks pblock_"<<slot_names[a]<< "] -add {RAMB36_X" <<slices_in_slot[a][2].slice_x1<<"Y" <<\
                     slices_in_slot[a][2].slice_y1 <<":" <<"RAMB36_X"<<slices_in_slot[a][2].slice_x2<<"Y"<<slices_in_slot[a][2].slice_y2 << "}" <<endl;\
         }\
-        if (US_FPGA == 1) \
+        if (US_FPGA == 1) { \
         if((*from_fp_solver->dsp_from_solver)[a] != 0) {\
         write_xdc << "resize_pblock [get_pblocks pblock_"<<slot_names[a]<< "] -add {DSP48E2_X" <<slices_in_slot[a][3].slice_x1<<"Y" <<\
                     slices_in_slot[a][3].slice_y1 <<":" <<"DSP48E2_X"<<slices_in_slot[a][3].slice_x2<<"Y"<<slices_in_slot[a][3].slice_y2 << "}" <<endl;\
-        }\
-        else \
+        }}\
+        else {\
         if((*from_fp_solver->dsp_from_solver)[a] != 0) {\
         write_xdc << "resize_pblock [get_pblocks pblock_"<<slot_names[a]<< "] -add {DSP48_X" <<slices_in_slot[a][3].slice_x1<<"Y" <<\
                     slices_in_slot[a][3].slice_y1 <<":" <<"DSP48_X"<<slices_in_slot[a][3].slice_x2<<"Y"<<slices_in_slot[a][3].slice_y2 << "}" <<endl;\
-        }\
+        }}\
         write_xdc << "set_property RESET_AFTER_RECONFIG true [get_pblocks pblock_"<< slot_names[a] <<"]" <<endl;\
         write_xdc << "set_property SNAPPING_MODE OFF [get_pblocks pblock_"<< slot_names[a] <<"]" <<endl;\
         write_xdc <<endl <<endl;\
